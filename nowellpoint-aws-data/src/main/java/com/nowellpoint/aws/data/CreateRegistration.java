2199bb482941c44fa8f22bd88226317afde3e40e
John Herson <john.d.herson@gmail.com>
2015-11-18 22:23:52 -0500

    added a new project nowellpoint-aws-data for all database services

---
+++ nowellpoint-aws-data/src/main/java/com/nowellpoint/aws/data/CreateRegistration.java
@@ -0,0 +1,8 @@
+package com.nowellpoint.aws.data;
+
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class CreateRegistration implements RequestHandler<GetIdentityRequest, GetIdentityResponse> {
+	
+	
+}
