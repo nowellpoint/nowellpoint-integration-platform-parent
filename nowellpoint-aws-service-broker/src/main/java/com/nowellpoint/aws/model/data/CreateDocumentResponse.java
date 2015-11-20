3101b226ecf20655106527fb1c97bb9feab948e5
John Herson <john.d.herson@gmail.com>
2015-11-19 21:39:49 -0500

    renaming of modules is complete

--- nowellpoint-aws-service-broker/src/main/java/com/nowellpoint/aws/model/data/CreateDocumentResponse.java
+++
@@ -1,9 +0,0 @@
-package com.nowellpoint.aws.model.data;
-
-import com.nowellpoint.aws.model.AbstractResponse;
-
-public class CreateDocumentResponse extends AbstractResponse {
-
-	private static final long serialVersionUID = -6801344091393248864L;
-
-}
