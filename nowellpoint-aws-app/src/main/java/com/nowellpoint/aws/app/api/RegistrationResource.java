dac96e13a72ec41fccb98ff48c477be3e24b3934
John Herson <john.d.herson@gmail.com>
2015-11-30 23:38:41 -0500

    added a test authenticate function to web app

--- nowellpoint-aws-app/src/main/java/com/nowellpoint/aws/app/api/RegistrationResource.java
+++ nowellpoint-aws-app/src/main/java/com/nowellpoint/aws/app/api/RegistrationResource.java
@@ -5,8 +5,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.nowellpoint.aws.client.IdentityProviderClient;
+import com.nowellpoint.aws.model.idp.GetTokenRequest;
+import com.nowellpoint.aws.model.idp.GetTokenResponse;
+
 @Path("/registration")
 public class RegistrationResource {
+	
+	private static IdentityProviderClient identityProviderClient = new IdentityProviderClient();
 	
 	public RegistrationResource() {
 		
@@ -15,6 +21,20 @@ public class RegistrationResource {
 	@GET
     @Produces(MediaType.APPLICATION_JSON)
     public String findAll() {
-        return "wildfly-swarm-response";
+		
+		long startTime = System.currentTimeMillis();
+		
+		GetTokenRequest tokenRequest = new GetTokenRequest().withUsername(System.getenv("STORMPATH_USERNAME"))
+				.withPassword(System.getenv("STORMPATH_PASSWORD"));
+		
+		GetTokenResponse tokenResponse = identityProviderClient.authenticate(tokenRequest);
+			
+		String accessToken = tokenResponse.getToken().getAccessToken();
+			
+		System.out.println("Authenticating...success: " + tokenResponse.getToken().getStormpathAccessTokenHref());
+		
+		System.out.println("Authenticate: " + (System.currentTimeMillis() - startTime));
+		
+        return "Authenticating...success: " + tokenResponse.getToken().getStormpathAccessTokenHref();
     }
 }
