7d1e30ba70363736bd02f011a0bc9070783d5889
John Herson <john.d.herson@gmail.com>
2016-02-21 21:05:17 -0500

    removed all code related to mongodb lambda functions

--- nowellpoint-aws-core/src/main/java/com/nowellpoint/aws/model/data/DeleteDocumentResponse.java
+++
@@ -1,6 +0,0 @@
-package com.nowellpoint.aws.model.data;
-
-public class DeleteDocumentResponse extends AbstractDocumentResponse {
-
-	private static final long serialVersionUID = 6117232788854302357L;
-}
