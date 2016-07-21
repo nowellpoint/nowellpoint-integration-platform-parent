8a22c74c9ae515ff7870f7a6b948c59b95185cb3
John Herson <john.d.herson@gmail.com>
2016-07-20 23:31:18 -0400

    working on email verification message

--- nowellpoint-aws-test/src/test/java/com/nowellpoint/sendgrid/TestSendEmail.java
+++ nowellpoint-aws-test/src/test/java/com/nowellpoint/sendgrid/TestSendEmail.java
@@ -1,12 +1,27 @@
 package com.nowellpoint.sendgrid;
 
+import java.io.IOException;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.nowellpoint.aws.model.admin.Properties;
-
+import com.sendgrid.Content;
+import com.sendgrid.Email;
+import com.sendgrid.Mail;
+import com.sendgrid.Method;
+import com.sendgrid.Personalization;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
-import com.sendgrid.SendGridException;
+import com.stormpath.sdk.account.Account;
+import com.stormpath.sdk.account.AccountStatus;
+import com.stormpath.sdk.account.EmailVerificationToken;
+import com.stormpath.sdk.api.ApiKey;
+import com.stormpath.sdk.api.ApiKeys;
+import com.stormpath.sdk.application.Application;
+import com.stormpath.sdk.client.Client;
+import com.stormpath.sdk.client.Clients;
 
 public class TestSendEmail {
 	
@@ -17,24 +32,73 @@ public class TestSendEmail {
 	
 	@Test
 	public void sendEmail() {
-		SendGrid sendgrid = new SendGrid("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");
 		
-		SendGrid.Email email = new SendGrid.Email();
+		ApiKey apiKey = ApiKeys.builder()
+				.setId(System.getProperty(Properties.STORMPATH_API_KEY_ID))
+				.setSecret(System.getProperty(Properties.STORMPATH_API_KEY_SECRET))
+				.build();
+		
+		Client client = Clients.builder()
+				.setApiKey(apiKey)
+				.build();
+		
+		Application application = client.getResource(System.getProperty(Properties.STORMPATH_API_ENDPOINT).concat("/applications/")
+				.concat(System.getProperty(Properties.STORMPATH_APPLICATION_ID)), Application.class);
 
-	    email.addTo("john.d.herson@gmail.com");
-	    email.setFrom("administrator@nowellpoint.com");
-	    email.setSubject("Sending with SendGrid is Fun");
-	    email.setHtml("and easy to do anywhere, even with Java");
-	    email.addSubstitution(":name", new String[] {"John Herson"});
-	    email.setTemplateId("3e2b0449-2ff8-40cb-86eb-32cad32886de");
+        Account account = client.instantiate(Account.class);
 
+        //Set the account properties
+        account.setGivenName("Joe")
+                .setSurname("Quickstart_Stormtrooper")
+                .setUsername("tk421")  
+                .setEmail("tk421@stormpath.com")
+                .setPassword("Changeme1")
+                .setStatus(AccountStatus.UNVERIFIED);
+        
+
+        // Create the account using the existing Application object
+        account = application.createAccount(account);
+        
+        EmailVerificationToken emailVerificationToken = account.getEmailVerificationToken();
+        
+        account.delete();
+		
+		Email from = new Email();
+		from.setEmail("administrator@nowellpoint.com");
+		from.setName("Nowellpoint Support");
+	    
+	    Email to = new Email();
+	    to.setEmail("john.d.herson@gmail.com");
+	    to.setName("John Herson");
+	    
+	    Content content = new Content();
+	    content.setType("text/html");
+	    content.setValue("<html><body>some text here</body></html>");
+	    
+	    Personalization personalization = new Personalization();
+	    personalization.addTo(to);
+	    personalization.addSubstitution("%name%", "John Herson");
+	    personalization.addSubstitution("%emailVerificationToken%", emailVerificationToken.getHref());
+	    
+	    Mail mail = new Mail();
+	    mail.setFrom(from);
+	    mail.addContent(content);
+	    mail.setTemplateId("3e2b0449-2ff8-40cb-86eb-32cad32886de");
+	    mail.addPersonalization(personalization);
+
+	    SendGrid sendgrid = new SendGrid(System.getProperty(Properties.SENDGRID_API_KEY));
+	    
+	    Request request = new Request();
 	    try {
-			SendGrid.Response response = sendgrid.send(email);
-			System.out.println(response.getMessage());
-			System.out.println(response.getStatus());
-		} catch (SendGridException e) {
-			e.printStackTrace();
-		}
+	      request.method = Method.POST;
+	      request.endpoint = "mail/send";
+	      request.body = mail.build();
+	      Response response = sendgrid.api(request);
+	      System.out.println(response.statusCode);
+	      System.out.println(response.body);
+	      System.out.println(response.headers);
+	    } catch (IOException e) {
+	    	e.printStackTrace();
+	    }
 	}
-
 }
