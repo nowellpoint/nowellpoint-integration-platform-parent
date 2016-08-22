cad03b9e3b5e036e8fea97d55c20d68f9ed31c77
John Herson <john.d.herson@gmail.com>
2016-08-22 17:16:26 -0400

    testing most efficient way to save a metadata backup

--- nowellpoint-aws-sforce/src/test/java/com/nowellpoint/client/sforce/TestAutheticators.java
+++ nowellpoint-aws-sforce/src/test/java/com/nowellpoint/client/sforce/TestAutheticators.java
@@ -3,6 +3,7 @@ package com.nowellpoint.client.sforce;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,8 +15,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.BeforeClass;
@@ -25,14 +28,20 @@ import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CompleteMultipartUploadRequest;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.InitiateMultipartUploadRequest;
 import com.amazonaws.services.s3.model.InitiateMultipartUploadResult;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PartETag;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.UploadPartRequest;
+import com.amazonaws.util.IOUtils;
+import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nowellpoint.aws.http.HttpRequestException;
 import com.nowellpoint.aws.http.HttpResponse;
@@ -73,114 +82,98 @@ public class TestAutheticators {
 			
 			Client client = new Client();
 			
-			List<PartETag> partETags = new ArrayList<PartETag>();
+			List<DescribeSobjectResult> describeResults = new ArrayList<DescribeSobjectResult>();
 			
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-hh:mm:ss");
 			
 			String keyName = String.format("%s/%s", response.getIdentity().getOrganizationId(), sdf.format(Date.from(Instant.now())));
 			
-			InitiateMultipartUploadRequest initRequest = new InitiateMultipartUploadRequest("aws-microservices", keyName);
-			InitiateMultipartUploadResult initResponse = s3client.initiateMultipartUpload(initRequest);
+			System.out.println(keyName);
 			
 			DescribeGlobalSobjectsRequest describeGlobalSobjectsRequest = new DescribeGlobalSobjectsRequest()
 					.setAccessToken(response.getToken().getAccessToken())
 					.setSobjectsUrl(response.getIdentity().getUrls().getSobjects());
 			
 			DescribeGlobalSobjectsResult describeGlobalSobjectsResult = client.describeGlobal(describeGlobalSobjectsRequest);
-			
+					
 			ExecutorService executor = Executors.newFixedThreadPool(describeGlobalSobjectsResult.getSobjects().size());
 			
-			int i = 0;
+			List<Future<DescribeSobjectResult>> tasks = new ArrayList<Future<DescribeSobjectResult>>();
+			
 			for (Sobject sobject : describeGlobalSobjectsResult.getSobjects()) {
-				System.out.println(sobject.getName());
-				executor.submit(new Task(
+				Future<DescribeSobjectResult> task = executor.submit(new Task(
 						response.getToken().getAccessToken(),
 						response.getIdentity().getUrls().getSobjects(),
 						sobject.getName(),
-						"aws-microservices",
-						s3client,
-						client,
-						keyName,
-						initResponse.getUploadId(),
-						i));
+						client));
 				
-				i++;
-				
+				tasks.add(task);
 			}
 			
 			executor.shutdown();
 			executor.awaitTermination(30, TimeUnit.SECONDS);
 			
+			for (Future<DescribeSobjectResult> task : tasks) {
+				describeResults.add(task.get());
+			}
+			
+			byte[] bytes = new ObjectMapper().writeValueAsBytes(describeResults);
+			
+			ObjectMetadata objectMetadata = new ObjectMetadata();
+			objectMetadata.setContentLength(bytes.length);
+			
+			s3client.putObject(new PutObjectRequest(
+					"aws-microservices",
+					keyName,
+					new ByteArrayInputStream(bytes),
+					objectMetadata));
+			
+			System.out.println("getting object");
+			GetObjectRequest getObjectRequest = new GetObjectRequest("aws-microservices", keyName);
+			S3Object s3object = s3client.getObject(getObjectRequest);
+			
+			System.out.println(IOUtils.toString(s3object.getObjectContent()));
+			
 		} catch (OauthException e) {
 			System.out.println(e.getStatusCode());
 			System.out.println(e.getError());
 			System.out.println(e.getErrorDescription());
-		} catch (InterruptedException e) {
+		} catch (AmazonClientException | IOException e) {
+	    	System.out.println(e.getMessage());
+	    } catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 	}
 }
 
-class Task implements Callable<Void> {
+class Task implements Callable<DescribeSobjectResult> {
 	
 	private String sessionId;
 	private String sobjectsUrl;
 	private String sobject;
-	private String bucketName;
-	private AmazonS3 s3client;
 	private Client client;
-	private String key;
-	private String uploadId;
-	private Integer partNumber;
 	
-	public Task(String sessionId, String sobjectsUrl, String sobject, String bucketName, AmazonS3 s3client, Client client, String key, String uploadId, Integer partNumber) {
+	public Task(String sessionId, String sobjectsUrl, String sobject, Client client) {
 		this.sessionId = sessionId;
 		this.sobjectsUrl = sobjectsUrl;
 		this.sobject = sobject;
-		this.bucketName = bucketName;
-		this.s3client = s3client;
 		this.client = client;
-		this.key = key;
-		this.uploadId = uploadId;
-		this.partNumber = partNumber;
 	}
 
 	@Override
-	public Void call() throws Exception {
+	public DescribeSobjectResult call() throws Exception {
 		DescribeSobjectRequest describeSobjectRequest = new DescribeSobjectRequest()
 				.withAccessToken(sessionId)
 				.withSobjectsUrl(sobjectsUrl)
 				.withSobject(sobject);
-				
-		try {
 
 		DescribeSobjectResult describeSobjectResult = client.describeSobject(describeSobjectRequest);
 
-		byte[] input = new ObjectMapper().writeValueAsBytes(describeSobjectResult);
-		
-		UploadPartRequest uploadRequest = new UploadPartRequest()
-	            .withBucketName(bucketName)
-	            .withKey(key)
-	            .withUploadId(uploadId)
-	            .withPartNumber(partNumber)
-	            .withInputStream(new ByteArrayInputStream(input))
-	            .withPartSize(input.length);
-		
-		PartETag tag = s3client.uploadPart(uploadRequest).getPartETag();
-		System.out.println(tag);
-
-		
-	    } catch (AmazonClientException e) {
-	    	//System.out.println(e.getErrorCode());
-	    	System.out.println(e.getMessage());
-	    	//System.out.println(e.getErrorMessage());
-	    } catch (Exception e) {
-	    	e.printStackTrace();
-	    }
-	    
-	    System.out.println("task: " + sobject);
-		
-		return null;
+		return describeSobjectResult;
 	}
-	
 }
