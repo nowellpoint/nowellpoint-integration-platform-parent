f4350b4d1c1c1a8bb4a61428cbf15a963fe2bba0
John Herson <john.d.herson@gmail.com>
2017-01-22 00:32:47 -0500

    added cache to AbstractAccountProfileService

--- nowellpoint-aws-api/src/main/java/com/nowellpoint/api/service/AbstractAccountProfileService.java
+++ nowellpoint-aws-api/src/main/java/com/nowellpoint/api/service/AbstractAccountProfileService.java
@@ -7,49 +7,68 @@ import javax.inject.Inject;
 import org.bson.types.ObjectId;
 
 import com.nowellpoint.api.model.domain.AccountProfile;
+import com.nowellpoint.aws.data.AbstractCacheService;
 import com.nowellpoint.mongodb.DocumentManager;
 import com.nowellpoint.mongodb.DocumentManagerFactory;
+import com.nowellpoint.mongodb.document.MongoDocument;
+import com.nowellpoint.util.Assert;
 
-abstract class AbstractAccountProfileService {
+abstract class AbstractAccountProfileService extends AbstractCacheService {
 	
 	@Inject
 	private DocumentManagerFactory documentManagerFactory;
 	
 	protected void create(AccountProfile accountProfile) {
+		MongoDocument document = accountProfile.toDocument();
 		DocumentManager documentManager = documentManagerFactory.createDocumentManager();
-		documentManager.insertOne(accountProfile.toDocument());
+		documentManager.insertOne(document);
+		set(accountProfile.getId(), document);
 	}
 	
 	protected void update(AccountProfile accountProfile) {
+		MongoDocument document = accountProfile.toDocument();
 		DocumentManager documentManager = documentManagerFactory.createDocumentManager();
-		documentManager.replaceOne(accountProfile.toDocument());
+		documentManager.replaceOne(document);
+		set(accountProfile.getId(), document);
 	}
 	
 	protected AccountProfile findById(String id) {
-		DocumentManager documentManager = documentManagerFactory.createDocumentManager();
-		com.nowellpoint.api.model.document.AccountProfile document = documentManager.findOne( com.nowellpoint.api.model.document.AccountProfile.class, new ObjectId( id ) );
+		com.nowellpoint.api.model.document.AccountProfile document = get(com.nowellpoint.api.model.document.AccountProfile.class, id);
+		if (Assert.isNull(document)) {
+			DocumentManager documentManager = documentManagerFactory.createDocumentManager();
+			document = documentManager.findOne( com.nowellpoint.api.model.document.AccountProfile.class, new ObjectId( id ) );
+			set(id, document);
+		}
 		AccountProfile accountProfile = new AccountProfile( document );
 		return accountProfile;
 	}
 	
 	protected AccountProfile findByAccountHref(String accountHref) {
-		DocumentManager documentManager = documentManagerFactory.createDocumentManager();
-		com.nowellpoint.api.model.document.AccountProfile document = documentManager.findOne(com.nowellpoint.api.model.document.AccountProfile.class, eq ( "accountHref", accountHref ) );
+		com.nowellpoint.api.model.document.AccountProfile document = get(com.nowellpoint.api.model.document.AccountProfile.class, accountHref);
+		if (Assert.isNull(document)) {
+			DocumentManager documentManager = documentManagerFactory.createDocumentManager();
+			document = documentManager.findOne(com.nowellpoint.api.model.document.AccountProfile.class, eq ( "accountHref", accountHref ) );
+			set(accountHref, document);
+		}
 		AccountProfile accountProfile = new AccountProfile( document );
 		return accountProfile;
 	}
 	
 	protected AccountProfile findByUsername(String username) {
-		DocumentManager documentManager = documentManagerFactory.createDocumentManager();
-		com.nowellpoint.api.model.document.AccountProfile document = documentManager.findOne(com.nowellpoint.api.model.document.AccountProfile.class, eq ( "username", username ) );
-		AccountProfile accountProfile = new AccountProfile(document);
+		com.nowellpoint.api.model.document.AccountProfile document = get(com.nowellpoint.api.model.document.AccountProfile.class, username);
+		if (Assert.isNull(document)) {
+			DocumentManager documentManager = documentManagerFactory.createDocumentManager();
+			document = documentManager.findOne(com.nowellpoint.api.model.document.AccountProfile.class, eq ( "username", username ) );
+			set(username, document);
+		}
+		AccountProfile accountProfile = new AccountProfile( document );
 		return accountProfile;
 	}
 	
 	protected AccountProfile findBySubscriptionId(String subscriptionId) {
 		DocumentManager documentManager = documentManagerFactory.createDocumentManager();
 		com.nowellpoint.api.model.document.AccountProfile document = documentManager.findOne(com.nowellpoint.api.model.document.AccountProfile.class, eq ( "subscription.subscriptionId", subscriptionId ) );
-		AccountProfile accountProfile = new AccountProfile(document);
+		AccountProfile accountProfile = new AccountProfile( document );
 		return accountProfile;
 	}
 }
