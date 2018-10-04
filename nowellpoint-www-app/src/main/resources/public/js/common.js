801dd96871378feeda6770a850b2b9bd6dc08525
John Herson <john.d.herson@gmail.com>
2018-10-03 23:10:24 -0400

    added sidebar menu

--- nowellpoint-www-app/src/main/resources/public/js/common.js
+++ nowellpoint-www-app/src/main/resources/public/js/common.js
@@ -1,9 +1,4 @@
-$("#menu-toggle").click(function (e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-});
-
-$( document ).ready(function(){
+$( document ).ready(function() {
     $('[data-toggle="popover"]').popover(); 
 });
 
@@ -16,6 +11,18 @@ $(document).ready(function () {
         var href = $(this).attr('href');
         if (path.substring(0, href.length) === href) {
             $(this).closest('li').addClass('active');
+        }
+    });
+});
+
+$(document).ready(function() {
+    $("#toggleSidebar").click(function() {
+        if ( $('#sidebar').width() == 0 ) {
+            $('#sidebar').animate({width: "250px"}, 200)
+            $('#content').animate({left: "250px"}, 200)
+        } else {
+            $('#sidebar').animate({width: "0"}, 200)
+            $('#content').animate({left: "0"}, 200)
         }
     });
 });
