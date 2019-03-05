a1dc316d4410bae1e60931cf75c131c344333b4e
John Herson <john_herson@gmail.com>
2019-03-04 22:51:55 -0500

    added defaults for Limits to prevent errors

--- nowellpoint-www-app/src/main/java/com/nowellpoint/console/model/AbstractLimits.java
+++ nowellpoint-www-app/src/main/java/com/nowellpoint/console/model/AbstractLimits.java
@@ -11,28 +11,116 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 @JsonSerialize(as = Limits.class)
 @JsonDeserialize(as = Limits.class)
 public abstract class AbstractLimits {
-	public abstract Limit getConcurrentAsyncGetReportInstances();
-	public abstract Limit getConcurrentSyncReportRuns();
-	public abstract Limit getDailyAnalyticsDataflowJobExecutions();
-	public abstract Limit getDailyAsyncApexExecutions();
-	public abstract Limit getDailyDurableGenericStreamingApiEvents();
-	public abstract Limit getDailyDurableStreamingApiEvents();
-	public abstract Limit getDailyWorkflowEmails();
-	public abstract Limit getDataStorageMB();
-	public abstract Limit getDurableStreamingApiConcurrentClients();
-	public abstract Limit getFileStorageMB();
-	public abstract Limit getHourlyAsyncReportRuns();
-	public abstract Limit getHourlyDashboardRefreshes();
-	public abstract Limit getHourlyDashboardResults();
-	public abstract Limit getHourlyDashboardStatuses();
-	public abstract Limit getHourlyODataCallout();
-	public abstract Limit getHourlySyncReportRuns();
-	public abstract Limit getHourlyTimeBasedWorkflow();
-	public abstract Limit getMassEmail();
-	public abstract Limit getMonthlyPlatformEvents();
-	public abstract Limit getPackage2VersionCreates();
-	public abstract Limit getSingleEmail();
-	public abstract Limit getStreamingApiConcurrentClients();
+	
+	@Value.Default
+	public Limit getConcurrentAsyncGetReportInstances() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getConcurrentSyncReportRuns() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getDailyAnalyticsDataflowJobExecutions() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getDailyAsyncApexExecutions() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getDailyDurableGenericStreamingApiEvents() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getDailyDurableStreamingApiEvents() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getDailyWorkflowEmails() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getDataStorageMB() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getDurableStreamingApiConcurrentClients() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getFileStorageMB() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getHourlyAsyncReportRuns() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getHourlyDashboardRefreshes() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getHourlyDashboardResults() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getHourlyDashboardStatuses() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getHourlyODataCallout() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getHourlySyncReportRuns() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getHourlyTimeBasedWorkflow() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getMassEmail() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getMonthlyPlatformEvents() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getPackage2VersionCreates() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getSingleEmail() {
+		return Limit.builder().build();
+	}
+	
+	@Value.Default
+	public Limit getStreamingApiConcurrentClients() {
+		return Limit.builder().build();
+	}
 	
 	public static Limits of(com.nowellpoint.client.sforce.model.Limits source) {
 		return source == null ? Limits.builder().build() : Limits.builder()
